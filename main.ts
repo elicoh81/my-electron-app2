f7929a9e10687d0e4556c4e6e51aa54d220f1b3c
Eli Cohen <eli.cohen.ws@gmail.com>
2023-06-17 11:46:55 +0300

    added electron reloading

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
+
+const path = require('path')
+const env = process.env.NODE_ENV || 'development';
+
+// If development environment
+if (env === 'development') {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+        hardResetMethod: 'exit'
+    });
+}
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -17,7 +27,9 @@ app.whenReady().then(() => {
     createWindow()
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
     })
 });
 
